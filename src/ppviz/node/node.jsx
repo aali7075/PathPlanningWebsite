7d4f66665fe406f314dacea661ddcfb7867607af
aaron <aali7075@colorado.edu>
2020-05-17 17:58:19 -0700

    added the node component and style

---
+++ src/ppviz/node/node.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+import "./Node.css";
+
+export default class Node extends Component {
+  //export is an include for class Node
+
+  render() {
+    //allows us to share code between react Components
+    const {
+      //set constant because our properties will always be 8
+      row_,
+      col_,
+      is_wall_,
+      is_end_,
+      is_start_,
+      on_mouse_down_,
+      on_mouse_up_,
+      on_mouse_enter_,
+    } = this.props;
+    let special_node_class = is_start_
+      ? "node-start"
+      : is_end_
+      ? "node-end"
+      : ""; //default case
+
+    return (
+      <div id={`node-${row}-${col}`} className={`node ${extraClassName}`}></div>
+    );
+  }
+}
